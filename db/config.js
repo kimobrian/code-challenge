8203f3d4d8a0dca0ee30222d0547cca52930adf2
kimobrian <bkim19302@gmail.com>
2020-07-27 16:42:44 +0300

    chore: fix URL settings

--- db/config.js
+++ db/config.js
@@ -1,24 +1,20 @@
 require("dotenv").config();
-const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
+// const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
 
 module.exports = {
   "development": {
-    "database": DB_NAME,
+    "use_env_variable": "DATABASE_URL",
     "dialect": "mysql",
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "host": DB_HOST,
     "logging": console.log
   },
   "test": {
-    "database": DB_NAME,
+    "use_env_variable": "DATABASE_URL",
     "dialect": "mysql",
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
     "logging": console.log
   },
   "production": {
-    "use_env_variable": "mysql://bc6ff727c77f88:4d953742@us-cdbr-east-02.cleardb.com/heroku_815ec254b4beb0c?reconnect=true",
-    "dialect": "mysql"
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "mysql",
+    "logging": console.log
   }
 };
